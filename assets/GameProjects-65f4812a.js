d4467cb8946053b4b9261498f6ae323cd2392895
lleven <ohayou@live.se>
2024-10-09 14:17:44 +0200

    initial commit

---
+++ assets/GameProjects-65f4812a.js
@@ -0,0 +1,100 @@
+import{P as d}from"./ProjectsList-96cc8a8e.js";import{f as p,_ as g,r as h,c as m,d as u,a as e,b as v,o as b}from"./index-b7c8b944.js";class t{constructor(r,i,o,n,l="#000000",a=!1,c=!1){this.id=r,this.name=i,this.htmlDescription=n,this.iconUrl=o,this.isHigh=a,this.isWide=c,this.accentColor=l}}const f=[new t("project-1","Alone in the Dark","img/projects/AitD_Keyart.jpg",`
+    <div class="paragraph">
+     <strong>Alone in the Dark</strong>. In this love letter to the ground-breaking original, THQ Nordic and Pieces Interactive present you a re-imagination of the classic survival horror game, Alone in the Dark. Set in the gothic American south, you control one of our two protagonists: Play as Edward Carnby or Emily Hartwood to explore your environments, fight monsters, solve puzzles and uncover the true secret of Derceto Manor…
+
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube-nocookie.com/embed/6lyDlYbbqkY" frameborder="0" allowfullscreen></iframe>
+    </div>
+    
+
+    <div class="paragraph">
+        I mainly worked with Level Design on Alone in the Dark. My duties included:
+        <ul>
+        <li>Being responsible for the levels and building them from the ground up</li>
+        <li>Creating the layouts with whiteboxing/blockmeshing</li>
+        <li>Scripting events and combat encounters</li>
+        <li>Puzzle design and scripting</li>
+        <li>Working in close colaboration with artists, animators, programmers, etc.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+    <div class="notice">
+        Game available on <a href="https://store.steampowered.com/app/1310410/Alone_in_the_Dark/" target="_blank">Steam</a>,
+         <a href="https://store.playstation.com/concept/10005283" target="_blank">PS5</a> and 
+         <a href="https://www.xbox.com/en-US/games/store/alone-in-the-dark/9MW67WG48XVX" target="_blank">XBOX</a>
+    </div>
+    </div>
+
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/Convent04.jpg" alt="AitD Convent01" />
+        <img class="pc-screenshot" src="img/projects/Steamboat01.jpg" alt="AitD Steamboat01" />
+        <img class="pc-screenshot" src="img/projects/Steamboat03.jpg" alt="AitD Steamboat02" />
+        <img class="pc-screenshot" src="img/projects/FrenchQuarter02.jpg" alt="AitD FrenchQuarter01" />
+    </div>
+    `,"#407e5b",!0),new t("project-2","Titan Quest: Atlantis","img/projects/tq_atlantis_square.png",`
+    <div class="paragraph">
+        <strong>Titan Quest: Atlantis</strong> is the third expansion for Titan Quest released in 2019. This new expansion adds a sixth act, along with Tartarus Endless Mode - a wave based challenge mode. There are also expanded masteries: unlock a new tier of skills for each mastery!
+        <!--
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@adonyi-gabor-604571">Adonyi Gábor</a>.
+        -->
+        </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube-nocookie.com/embed/VIyZi9-PrmU" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        I mainly worked with Level Design on Atlantis. My duties included:
+        <ul>
+        <li>Being responsible for the levels and building them from the ground up</li>
+        <li>Creating the layouts as well as set dressing</li>
+        <li>Scripting events and combat encounters</li>
+        <li>Quest design and scripting</li>
+        <li>Working in close colaboration with artists, animators, programmers, etc.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+    <div class="notice">
+        Game available on <a href="https://store.steampowered.com/app/1071200/Titan_Quest_Atlantis/" target="_blank">Steam</a>
+    </div>
+    </div>
+<!--
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/tq_atlantis_square.png alt="Atlantis" />
+    </div>
+-->
+    `,"#406c7e"),new t("project-3","Titan Quest: Ragnarök","img/projects/tq_ragnarok_square.png",`
+    <div class="paragraph">
+        <strong>Titan Quest: Ragnarök</strong> is the second expansion to the acclaimed Anniversary Edition of Titan Quest, which adds a fifth act of unprecedented scope, along with many new and requested features including new weapon types and a 10th Mastery for your characters to learn.
+        <!--
+        <br/>Image by <a target="_blank" href="https://www.pexels.com/fr-fr/@miphotography">Miesha Maiden</a>.
+        -->
+    </div>
+    <div class="paragraph center">
+        <iframe class="youtube" src="https://www.youtube-nocookie.com/embed/bsW38Ed1ABU" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <div class="paragraph">
+        I mainly worked with Level Design on Ragnarök. My duties included:
+        <ul>
+        <li>Being responsible for the levels and building them from the ground up</li>
+        <li>Creating the layouts as well as set dressing</li>
+        <li>Scripting events and combat encounters</li>
+        <li>Quest design and scripting</li>
+        <li>Working in close colaboration with artists, animators, programmers, etc.</li>
+        </ul>
+    </div>
+
+    <div class="paragraph">
+        <div class="notice">
+        Game available on <a href="https://store.steampowered.com/app/741350/Titan_Quest_Ragnark/" target="_blank">Steam</a>
+        </div>
+    </div>
+<!--
+    <div class="paragraph center">
+        <img class="pc-screenshot" src="img/projects/tq_ragnarok_square.png alt="Ragnarok" />
+    </div>
+-->
+    `,"#7e4f40")],w=p({name:"GameProjects",components:{ProjectsList:d},data:function(){return{projects:f}}}),_=e("h1",null,"Published Projects",-1),y=e("div",{style:{"margin-bottom":"30px"}}," These are some games I've made or heavily contributed to. ",-1),k=e("div",{style:{"margin-top":"20px"}},[v(" There is more to see on my "),e("a",{target:"_blank",href:"https://www.artstation.com/andreas_larsson"},"ArtStation.")],-1);function j(s,r,i,o,n,l){const a=h("ProjectsList");return b(),m("div",null,[_,y,u(a,{projects:s.projects},null,8,["projects"]),k])}const x=g(w,[["render",j]]);export{x as default};
